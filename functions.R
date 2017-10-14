9420d471a7fbe95d026b2318070c1e28c1b38c6b
Isabel Hojman <isabelhojman@gmail.com>
2017-10-14 20:59:46 +0200

    add 1 to 3b

---
+++ functions.R
@@ -0,0 +1,175 @@
+# Isabel Hojman Prati
+# Assignment 2
+
+
+
+#EXERCISE 1
+
+# Sum values in a column of a data frame.
+#
+# ARGUMENTS:
+# d: a data frame or tibble
+# var: the name of a column of d, provided as a string
+#
+# RETURN VALUE:
+# if the specified column exists and contains numbers, returns the sum of
+# all values in the column; otherwise, returns NULL
+sum_column <- function(d, var) {
+  # Set a default value to return
+  result <- NULL
+  x <- d[[var]] # Remember, as we showed in class: there are two ways
+  # to access a column, and this is one; you should try
+  # and figure out why the other way won't work here,
+  # but that's not part of the homework
+  if (!is.null(x)) { # This tests to see whether the column exists in
+    # d; again, you should try and find a way to test
+    # this out for yourself to verify that it's true,
+    # but that's not part of the homework
+    # YOUR CODE HERE: if x contains numbers, set the variable
+    # result to be the sum of the values in x
+    #
+    # You will need to do a bit of research in order to figure out how
+    # to test whether a vector contains numbers.
+    if (is.numeric(x)) {
+      result <- sum(x)
+    }
+    
+  }
+  # YOUR CODE HERE: return the result
+  return(result)
+}
+
+
+# Sum values in a vector.
+#
+# ARGUMENTS:
+# x: a vector
+#
+# RETURN VALUE:
+# if the vector contains numbers, returns the sum of
+# all values; otherwise, returns NULL
+#
+my_sum <- function(x) {
+  #print(x)
+  # Set a default value to return
+  result <- NULL
+  # Test if x is numeric
+  if (is.numeric(x)) {
+    result <- 0.0
+    # Loop for to sum each element of x
+    for (ele in x) {
+      result <- result + ele
+    }
+  }
+  # Return sum
+  return(result)
+}
+
+#Sum divided by
+#
+#ARGUMENTS:
+# x: a vector
+# k: a number
+#
+#RETURN VALUE:
+#the sum of the elements of x divided by the number k. 
+#If either x or k are non-numeric, it should return NULL.
+sum_divided_by <- function(x, k) {
+  if (!is.numeric(x) || !is.numeric(k)) {
+    result <- NULL
+  }
+  else {
+    result <- my_sum(x)
+    result <- result / k
+  }
+  return(result)
+}
+
+
+#Calculate mean of a vector
+#
+#ARGUMENTS:
+# x: a vector
+#
+#RETURN VALUE:
+#the mean of a vector
+my_mean <- function(x) {
+  return(sum_divided_by(x, length(x)))
+}
+
+#EXERCISE 2
+
+# Return a violin plot.
+#
+# ARGUMENTS:
+# d: a data frame or tibble
+# var: the name of a column of d containing the dependent variable, provided as a
+#     string
+# grouping_var: the name of a column of d containing a grouping variable,
+#               provided as a string
+#
+# RETURN VALUE:
+# A ggplot plot object containing a violin plot, grouped by the values
+# of the grouping variable.
+#
+grouped_violin_plot <- function(d, var, grouping_var) {
+  # Create the base ggplot object
+  p <- ggplot2::ggplot(d, ggplot2::aes_string(y=var,
+                                              x=grouping_var,
+                                              fill=grouping_var))
+  p <- p + ggplot2::geom_violin(colour="black")
+  # YOUR CODE HERE: Create a violin plot
+  return(p) 
+}
+
+#EXERCISE 3
+
+# Difference in the medians between two groups.
+#
+# ARGUMENTS:
+# d: a data frame or tibble
+# var: the name of a column of d containing the dependent variable, provided as a string
+# grouping_var: the name of a column of d containing a grouping variable, provided as a string
+# group1: the value of grouping_var that corresponds to the first group
+# group2: the value of grouping_var that corresponds to the second group
+#
+# RETURN VALUE:
+# The median value of var for the first group, minus the median value of var for the second
+# group.
+#
+difference_in_medians <- function(d, var, grouping_var, group1, group2) {
+  d_1 <- dplyr::filter(d, get(grouping_var) == group1)
+  d_2 <- dplyr::filter(d, get(grouping_var) == group2)
+  # YOUR CODE HERE: assign the difference in the medians to to the variable 'result'
+  result <- median(d_1[[var]]) - median(d_2[[var]])
+  return(result)
+}
+
+
+# Randomize the order of a column.
+#
+# ARGUMENTS:
+# d: a data frame or tibble
+# var: the name of a column of d containing the variable to randomize,
+#      provided as a string
+#
+# RETURN VALUE:
+# A data frame or tibble exactly the same as d, except with the order of
+# var permuted randomly.
+#
+randomize <- function(d, var) {
+  d[[var]] <- sample(d[[var]]) # generate a shuffled version of d[[var]]
+  return(d) 
+
+}
+
+
+
+
+
+
+
+
+
+
+
